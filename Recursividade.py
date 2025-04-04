5b250c613179fad6404470333a75f6f7da5a45e2
Artur <110943874+bogoartur@users.noreply.github.com>
2025-04-04 19:30:15 -0300

    Add files via upload

--- Recursividade.py
+++ Recursividade.py
@@ -1,12 +1,29 @@
+arquivo = open("passados.txt", "r")
+
+sequencia = []
+
+for line in arquivo:
+    sequencia.append(int(line))
+arquivo.close()
+
+arquivo = open("passados.txt", "a")
 
 
 def fibonacci(pos):
-    if pos <= 1:
-        return pos
+    if pos <= len(sequencia):
+        return sequencia[pos - 1]
     else:
-        for n in range(pos):
-            return fibonacci(pos-2) + fibonacci(pos-1)
+        if pos <= 1:
+            return pos
+        else:
+            for n in range(pos - len(sequencia)):
+                valor = fibonacci(pos - 1) + fibonacci(pos - 2)
+                sequencia.append(valor)
+                arquivo.write(str(sequencia[-1]) + "\n")
+                return valor
 
 pos = int(input("Digite a posicao que quer saber o fibonacci: "))
-resultado = fibonacci(pos   )
 print ("O elemento de fibonacci na posicao ", pos, " é ", fibonacci(pos))
+
+
+
