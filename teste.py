45173af638fe3608804cba90bfeeadd6f5e39f00
Artur <110943874+bogoartur@users.noreply.github.com>
2025-04-11 20:09:00 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,13 @@
+tabuleiro = [["X", "X", "X", "X", "X", "X", "X", "X"], ["X", "X", "X", "X", "X", "X", "X", "X"], ["X", "X", "X", "X", "X", "X", "X", "X"], ["X", "X", "X", "X", "X", "X", "X", "X"], ["X", "X", "X", "X", "X", "X", "X", "X"], ["X", "X", "X", "X", "X", "X", "X", "X"], ["X", "X", "X", "X", "X", "X", "X", "X"], ["X", "X", "X", "X", "X", "X", "X", "X"]]
+
+posAtual = [0, 0]
+
+posicoesRainhas = [[], [], [], [], [], [], [], []]
+
+for linha in tabuleiro:
+    if linha[posAtual] != "R" and :
+    for n in range(8):
+
+
+
+
